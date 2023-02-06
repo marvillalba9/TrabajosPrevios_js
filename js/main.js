265d4ed3b43f0451c0a6f095fafda115ba01cba9
marvillalba <maruja.villalba@gmail.com>
2023-02-05 22:37:18 -0300

    actualizacion de pre entrega 2

--- js/main.js
+++ js/main.js
@@ -72,16 +72,19 @@ const productos = [
         precio: 2100,
     },
 ]
-let carritoDeCompras = [];
 
+let carrito = [];
+const envio1 = 800;
+const envio2 = 500;
+let subtotal = 0;
 
 
 for (let i = 0; i !== "FIN"; i++) {
-    let productoElegido = prompt ("Ingrese el producto elegido (Tortas o Cupcakes), para ver la lista de productos disponibles ingrese LISTA, para cerrar el pedido ingrese FIN");
+    let producto = prompt ("Ingrese el producto elegido (Tortas o Cupcakes), para ver la lista de productos disponibles ingrese LISTA, para cerrar el pedido ingrese FIN");
 
-    if (productoElegido === "Cupcakes") {
-        let saborElegido = prompt ("Ingrese el sabor elegido: Limon, Chocolate, Vainilla o Red Velvet");
-            switch (saborElegido) {
+    if (producto === "Cupcakes" || producto === "Tortas") {
+        let sabor = prompt ("Ingrese el sabor elegido, las opciones para Cupcakes son: Limon, Chocolate, Vainilla o Red Velvet; las opciones para Tortas son: Brownie, Chocotorta, Cheesecake de Frutos Rojos, Cheesecake de Chocolate, Lemon Pie, Mousse de Frutilla, Rogel o Crumble de Manzana.");
+            switch (sabor) {
                 case "Chocolate":
                 case "Limon":
                     precio = 50;
@@ -92,17 +95,6 @@ for (let i = 0; i !== "FIN"; i++) {
                 case "Vainilla":
                     precio = 40;
                     break;
-            }
-        let cantidad = parseInt (prompt ("Ingrese la cantidad deseada"));
-        let sumaCupcakes = cantidad*precio;
-        alert ("Su subtotal por la compra de Cupcakes es de: $ " + sumaCupcakes);
-
-        // carritoDeCompras.push ({productoElegido, sabor, cantidad, precio});
-        // alert (`Su carrito de compras contiene: ${carritoDeCompras}`);
-
-    }     else if (productoElegido === "Tortas") {
-        let sabor = prompt ("Ingrese el sabor elegido: Brownie, Chocotorta, Cheesecake de Frutos Rojos, Cheesecake de Chocolate, Lemon Pie, Mousse de Frutilla, Rogel o Crumble de Manzana");
-            switch (sabor) {
                 case "Brownie":
                 case "Chocotorta":
                     precio = 1800;
@@ -121,57 +113,40 @@ for (let i = 0; i !== "FIN"; i++) {
                     break;
             }
         let cantidad = parseInt (prompt ("Ingrese la cantidad deseada"));
-        let sumaTortas = cantidad*precio;
-        alert ("Su subtotal por la compra de Tortas es de: $ " + sumaTortas);
+        subtotal = cantidad*precio;
+        alert ("Su subtotal por la compra es de: $ " + subtotal);
 
-        // carritoDeCompras.push ({productoElegido, sabor, cantidad, precio});
-        // console.log (carrito);
-        }   else if (productoElegido === "LISTA") {
+        carrito.push ({producto, sabor, cantidad, precio});
+        console.log (carrito);
+
+        let consultaEnvio = prompt ("Desea agregar envio a domicilio (SI/NO)?")
+            function calcularEnvio () {
+                if (consultaEnvio === "SI" && subtotal <= 1000) {
+                let sumaConEnvio = subtotal + envio1;
+                alert ("Su total a pagar es: $ " + sumaConEnvio);
+            } else if (consultaEnvio === "SI" && subtotal <= 3000) {
+                let sumaConEnvio = subtotal + envio2;
+                alert ("Su total a pagar es: $ " + sumaConEnvio);
+            } else if (consultaEnvio === "SI" && subtotal > 3000) {
+                alert ("Su envío esta bonificado");
+            } else {
+                alert ("Ponerse en contacto con nosotros para coordinar el retiro de los productos");
+            }
+        }
+        calcularEnvio ();
+
+    }    else if (producto === "LISTA") {
             let listaDeProductos = productos.map ((productos) => " " + productos.tipoDeProducto + " " + productos.sabor + " Precio: $ " + productos.precio)
             alert (`Nuestros productos son ${listaDeProductos}`)
             break;
     
-        } else {
+        } else if (producto === "FIN") {
             alert ("Gracias por visitar nuestra pagina");
             break;
         }
-
-        productoElegido = prompt ("Desea seguir comprando? Escriba Tortas, Cupcakes o FIN para salir");
-    
-        // carritoDeCompras.forEach ((carritoFinal) => {
-        //     alert (`El carrito contiene: ${carritoFinal.tipoDeProducto}, ${carritoFinal.sabor}, ${carritoFinal.precio}, ${carritoFinal.cantidad}`)
-        // })
-
-
 }
 
 
-
-    
-
-
-
-
-
-
-// let subtotal = sumaTortas + sumaCupcakes;
-// const envio1 = 800;
-// const envio2 = 500;
-// const envio3 = 0;
-
-// let consultaEnvio = prompt ("Desea agregar envio a domicilio (SI/NO)?")
-
-// if (consultaEnvio === "SI" && Subtotal <= 1000) {
-//     let sumaConEnvio = subtotal + envio1;
-//     alert ("Su total a pagar es: $ " + sumaConEnvio);
-// } else if (consultaEnvio === "SI" && Subtotal <= 3000) {
-//     let sumaConEnvio = subtotal + envio2;
-//     alert ("Su total a pagar es: $ " + sumaConEnvio);
-// } else if (consultaEnvio === "SI" && Subtotal > 3000) {
-//     alert ("Su envío esta bonificado");
-// } else {
-//     alert ("Ponerse en contacto con nosotros para coordinar el restiro de los productos");
-// }
 
 
 
