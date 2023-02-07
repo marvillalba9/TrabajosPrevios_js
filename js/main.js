542f683a01a9923bdf1e5a860608266f810faabd
marvillalba <maruja.villalba@gmail.com>
2023-02-07 00:44:51 -0300

    pre entrega js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ class Productos {
         this.sabor = sabor;
         this.precio = precio;
         this.vendido = false;
+        this.subtotal = 0;
+        this.total = 0;
     }
 
 }
@@ -28,14 +30,15 @@ console.log (productos);
 let carrito = [];
 const envio1 = 800;
 const envio2 = 500;
-let subtotal = 0;
+let cantidad = 0;
 
 
-for (let i = 0; i !== "FIN"; i++) {
-        let tipoDeProducto = prompt ("Ingrese el producto elegido (Tortas o Cupcakes), para ver la lista de productos disponibles ingrese LISTA, para cerrar el pedido ingrese FIN");
+
+for (let i = 0; i !== "SALIR"; i++) {
+        let tipoDeProducto = prompt ("Ingrese el producto elegido (Tortas o Cupcakes), para ver la lista de productos disponibles ingrese LISTA, para cerrar el pedido ingrese FIN o SALIR para abandonar la página.");
     
         if (tipoDeProducto === "Cupcakes" || tipoDeProducto === "Tortas") {
-            let sabor = prompt ("Ingrese el sabor elegido, las opciones para Cupcakes son: Limon, Chocolate, Vainilla o Red Velvet; las opciones para Tortas son: Brownie, Chocotorta, Cheesecake de Frutos Rojos, Cheesecake de Chocolate, Lemon Pie, Mousse de Frutilla, Rogel o Crumble de Manzana.");
+            let sabor = prompt ("Ingrese el sabor elegido, las opciones para Cupcakes son: Limon, Chocolate, Vainilla o Red Velvet; las opciones para Tortas son: Brownie, Chocotorta, Cheesecake de Frutos Rojos, Cheesecake de Chocolate, Lemon Pie, Mousse de Frutilla, Rogel o Crumble de Manzana.");    
                 switch (sabor) {
                     case "Chocolate":
                     case "Limon":
@@ -64,41 +67,45 @@ for (let i = 0; i !== "FIN"; i++) {
                         precio = 2500;
                         break;
                 }
-                 
-            let cantidad = parseInt (prompt ("Ingrese la cantidad deseada"));
-            subtotal = cantidad*precio;
-            alert (`Su subtotal por la compra es de: $ ${subtotal}`);
-  
+            cantidad = parseInt (prompt ("Ingrese la cantidad deseada"));
+              
             carrito.push ({tipoDeProducto, sabor, cantidad, precio});
-            for (const items of carrito) {
-                alert(`Su carrito contiene los siguientes items:\n Tipo de Producto: ${items.tipoDeProducto}\nSabor: ${items.sabor}\nPrecio unitario: ${items.precio}\nCantidad: ${items.cantidad}`);
-            }
+            console.log (carrito);
 
 
-            let consultaEnvio = prompt ("Desea agregar envio a domicilio (SI/NO)?")
-                function calcularEnvio () {
-                    if (consultaEnvio === "SI" && subtotal <= 1000) {
-                    let sumaConEnvio = subtotal + envio1;
-                    alert ("Su total a pagar es: $ " + sumaConEnvio);
-                } else if (consultaEnvio === "SI" && subtotal <= 3000) {
-                    let sumaConEnvio = subtotal + envio2;
-                    alert ("Su total a pagar es: $ " + sumaConEnvio);
-                } else if (consultaEnvio === "SI" && subtotal > 3000) {
-                    alert ("Su envío esta bonificado");
-                } else {
-                    alert ("Ponerse en contacto con nosotros para coordinar el retiro de los productos");
-                }
-            }
-            calcularEnvio ();
-      
         } else if  (tipoDeProducto === "LISTA") {
                 for (const producto of productos) {
                     alert(`Tipo de Producto: ${producto.tipoDeProducto}\nSabor: ${producto.sabor}\nPrecio: ${producto.precio}\n`);
                 }
 
-        } else {
-                alert ("Gracias por visitar nuestra pagina");
-                break;
+        } else if (tipoDeProducto === "FIN") {
+
+            carrito.forEach((carritoFinal) => {
+                alert(`Su carrito contiene los siguientes items:\n Tipo de Producto: ${carritoFinal.tipoDeProducto}\nSabor: ${carritoFinal.sabor}\nPrecio unitario: ${carritoFinal.precio}\nCantidad: ${carritoFinal.cantidad}\nSubtotal: ${carritoFinal.precio*carritoFinal.cantidad}`);
+            })
+            
+            const total = carrito.reduce((acc, elemento) => {return acc + elemento.precio * elemento.cantidad}, 0);
+            alert (`El total de su compra es de $: ${total}`);
+
+            let consultaEnvio = prompt ("Desea agregar envio a domicilio (SI/NO)?")
+                function calcularEnvio () {
+                    if (consultaEnvio === "SI" && total <= 1000) {
+                        let sumaConEnvio = total + envio1;
+                        alert ("Su total a pagar con envio es de: $ " + sumaConEnvio);
+                    } else if (consultaEnvio === "SI" && total <= 3000) {
+                        let sumaConEnvio = total + envio2;
+                        alert ("Su total a pagar con envio es de: $ " + sumaConEnvio);
+                    } else if (consultaEnvio === "SI" && total > 3000) {
+                        alert ("Su envío esta bonificado");
+                    } else {
+                        alert ("Ponerse en contacto con nosotros para coordinar el retiro de los productos");
+                    }
+                }
+                calcularEnvio ();
+
+            } else {
+            alert ("Gracias por visitar nuestra pagina");
+            break;
         }
     }
 
